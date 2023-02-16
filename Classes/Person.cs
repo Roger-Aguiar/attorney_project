eab95bbe2fc66affca9e48b2c871e82f67c2a180
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2023-02-16 17:48:45 -0300

    It creates the properties for Person class and creates the Person class

--- Classes/Person.cs
+++ Classes/Person.cs
@@ -1,4 +1,29 @@
 public class Person
 {
-    
+    private string? cPF;
+
+    public string? Name { get; set; }
+    public string? Nationality { get; set; }
+    public string? CivelStatus { get; set; }
+    public string? Profession { get; set; }
+    public string? Identity { get; set; }
+    public string? CPF 
+    { 
+        get 
+        {
+            return cPF;
+        } 
+        set 
+        {
+            cPF = value;
+        }
+    }
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+    public Address? Address { get; set; }
+
+    private bool ValidateCPF()
+    {        
+        return false;
+    }
 }
